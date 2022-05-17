779cc54036f35177a26ee331843156639414194a
tomorrowisnew_ <85917161+tomorroisnew@users.noreply.github.com>
2022-05-17 15:24:35 +0800

    Update main.cpp

--- x64/jni/src/main.cpp
+++ x64/jni/src/main.cpp
@@ -8,3 +8,18 @@
 #include <cstdlib>
 #include "exploits/DoSkillDamage.h"
 #import "include/Utils.h"
+
+void* hack_thread(void*) {
+    HackFunction();
+}
+
+void init(){
+    pthread_t ptid;
+    pthread_create(&ptid, NULL, hack_thread, NULL);
+}
+
+extern "C"
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    init();
+    return JNI_VERSION_1_6;
+}
