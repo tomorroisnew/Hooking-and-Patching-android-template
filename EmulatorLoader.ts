d219666317a2cbf2b0837d75136e4f30e0d5ed1d
tomorrowisnew_ <85917161+tomorroisnew@users.noreply.github.com>
2022-05-19 21:20:42 +0800

    Update EmulatorLoader.ts

--- EmulatorLoader.ts
+++ EmulatorLoader.ts
@@ -1,9 +1,12 @@
 const itf = Module.findExportByName("libhoudini.so", "NativeBridgeItf");
 console.log("itf Address: " + itf);
 console.log("NativeBridgeCallbacks version: " + itf.add(0).readUInt())
-var loadLibrary = itf.add(16).readPointer()
+var loadLibrary = itf.add(Process.pointerSize*2).readPointer()
 console.log("loadLibrary: " + loadLibrary);
 
+var getTrampoline = itf.add(Process.pointerSize*3).readPointer()
+
 var dlopen = new NativeFunction(loadLibrary, 'pointer', ['pointer', 'int'])
+var dlsym = new NativeFunction(getTrampoline, 'pointer', ['pointer', 'pointer', 'void', 'pointer'])
 
 dlopen(Memory.allocUtf8String("/data/local/tmp/libyeet.so"), 1)
