f3a18cd302b1e891dad64b7e41daa2c857ce7fa5
tomorrowisnew_ <85917161+tomorroisnew@users.noreply.github.com>
2022-05-21 12:40:46 +0800

    Update Loader.ts

--- Loader.ts
+++ Loader.ts
@@ -4,4 +4,4 @@ var dlerrora = Module.findExportByName(null, 'dlerror')
 var dlopen = new NativeFunction(dlopena, 'pointer', ['pointer', 'int'])
 var dlerror = new NativeFunction(dlerrora, 'pointer', [])
 
-dlopen("/data/data/com.dts.freefireth/libyeet.so")
+dlopen(Memory.allocUtf8String("/data/data/com.dts.freefireth/libyeet.so"), 2)
